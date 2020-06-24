4806bee878ef80d5417c8d30c9ce2280513d90df
TheMoodle <65734074+TheMoodle@users.noreply.github.com>
2020-06-24 18:58:53 -0400

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -5,6 +5,7 @@ then
 	echo "You Idot"
 
 else
+		#Text Variables
 		Red='\033[0;31m'
 		Orange='\033[0;33m'
 		NC='\033[0m'
@@ -14,23 +15,35 @@ else
 		Bold=$(tput bold)
 		Normal=$(tput sgr0)
 
-	echo -e ${White}' ___ ____  ____                         '
-	echo -e '|_ _|  _ \/ ___|_      _____  ___ _ __  '
-	echo -e ' | || |_) \___ \ \ /\ / / _ \/ _ \  _ \' 
-	echo -e ' | ||  __/ ___) \ V  V /  __/  __/ |_) |'
-	echo -e '|___|_|   |____/ \_/\_/ \___|\___| .__/ '
-	echo -e ${Red}${Bold}'               Made By: TheMoodle'${White}'|_|   '
+# Figlet of IPSweep
+echo -e ${White}"$(figlet IPSweep)" | sed '$d'
+echo -e ${Red}${Bold}'               Made By: TheMoodle'${White}'|_|   '
+		
+		#	Old Figlet Method
+		#	echo -e ${White}' ___ ____  ____                         '
+		#	echo -e '|_ _|  _ \/ ___|_      _____  ___ _ __  '
+		#	echo -e ' | || |_) \___ \ \ /\ / / _ \/ _ \  _ \' 
+		#	echo -e ' | ||  __/ ___) \ V  V /  __/  __/ |_) |'
+		#	echo -e '|___|_|   |____/ \_/\_/ \___|\___| .__/ '
+		#	echo -e ${Red}${Bold}'               Made By: TheMoodle'${White}'|_|   '
 
+#Purge Problematic Files
 	rm /tmp/ipsweep 2>/dev/null
 	rm /tmp/ipsweep.txt 2>/dev/null
 
+#Start Fping Scan
 	fping -a -g $1 2>/dev/null > /tmp/ipsweep
 	echo -e "${Yellow}Progress 1/2"
+
+#Start Nmap Scan
 	nmap -sP $1 2>/dev/null | grep "Nmap" | cut -d " " -f 5 | sed '1d;$d' >> /tmp/ipsweep
 	echo -e "${Yellow}Progress 2/2"
+
+#Sort and Concatenate Output File
 	sort -V -i /tmp/ipsweep | uniq > /tmp/ipsweep.txt
 	rm /tmp/ipsweep 2>/dev/null
 
+#Echo Out the Results
 	WC=`wc /tmp/ipsweep.txt | head -n1 | cut -d " " -f2`
 	echo -e ${Bold}${Red}'Completed: '${Blue}$WC ${Red}'Hosts Found!'
 	echo -e "${Normal}$(cat /tmp/ipsweep.txt)"
