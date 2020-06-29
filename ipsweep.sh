bd4a8bf4f0c665a03edc1c1a5020f2166fd66bf9
The Moodle <65734074+TheMoodle@users.noreply.github.com>
2020-06-28 22:37:00 -0400

    Added Spinner

--- ipsweep.sh
+++ ipsweep.sh
@@ -7,7 +7,8 @@ then
 
 else
 	
-		#Text Variables
+		#Variables
+		spinner=( '|' '/' '-' '\');
 		Red='\033[0;31m'
 		Orange='\033[0;33m'
 		NC='\033[0m'
@@ -16,6 +17,18 @@ else
 		Blue='\033[1;34m'
 		Bold=$(tput bold)
 		Normal=$(tput sgr0)
+
+#Loading Animation
+		spin(){
+		  while [ 1 ]
+		  do
+		    for i in ${spinner[@]};
+		    do
+		      echo -ne "${Yellow}\rInitializing $i";
+		      sleep 0.3;
+		    done;
+		  done
+		}
 
 # Figlet of IPSweep
 echo -e ${White}"$(figlet IPSweep)" | sed '$d'
@@ -26,19 +39,31 @@ echo -e ${Red}${Bold}'               Made By: TheMoodle'${White}'|_|   '
 	rm /tmp/ipsweep.txt 2>/dev/null
 
 #Start Scanning
-	echo -e "${Yellow}Initializing..."
+	spin &
+  	pid=$!
 	fping -a -g $1 2>/dev/null > /tmp/ipsweep &
+	P1=$!
 	nmap -sP $1 2>/dev/null | grep "Nmap" | cut -d " " -f 5 | sed '1d;$d' >> /tmp/ipsweep &
+	P2=$!
 	arp | sed '/incomplete/d' | cut -d " " -f 1 | sed '1d' >> /tmp/ipsweep &
-	wait
+	P3=$!
+	wait $P1 $P2 $P3
 
 #Sort and Concatenate Output File
 	sort -V -i /tmp/ipsweep | uniq > /tmp/ipsweep.txt
 	rm /tmp/ipsweep 2>/dev/null
 
 #Echo Out the Results
+	kill $pid 
 	WC=`wc /tmp/ipsweep.txt | head -n1 | cut -d " " -f2`
-	echo -e ${Bold}${Red}'Completed: '${Blue}$WC${Red} 'Hosts Found!'
+	echo -e ${Bold}${Red}'\rCompleted: '${Blue}$WC${Red} 'Hosts Found!'
 	echo -e "${NC}$(cat /tmp/ipsweep.txt)"
+fi
 
+if [ "$2" == "" ]
+	then
+		exit
+	else
+		cp /tmp/ipsweep.txt $2
+		exit
 fi
